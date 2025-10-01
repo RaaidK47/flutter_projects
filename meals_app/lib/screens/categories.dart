1e3f2c671555d7c1bbab7c099a55356b2483a181
RaaidK47 <raaid.khan47@gmail.com>
2025-10-01 20:36:54 +0500

    Added meals_app

---
+++ meals_app/lib/screens/categories.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your Category')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // Two Columns in Grid
+            childAspectRatio: 3 / 2, //   Aspect Ration
+            crossAxisSpacing: 20, // Horizontal Spacing
+            mainAxisSpacing: 20 // Vertical Spacing
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(category: category)
+          ],
+        ),
+      ),
+    );
+  }
+}
