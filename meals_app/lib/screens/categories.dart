06759fd38a8f2576f3eeb410a4579c68627653b4
RaaidK47 <raaid.khan47@gmail.com>
2025-10-02 21:52:20 +0500

    Add Meals Screen and MealDetails Screen in the Meals App

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    // Context is not globally available in Stateless Widget
+    // [Similar] Navigator.push(context, route);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +32,16 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisCount: 2, // Two Columns in Grid
             childAspectRatio: 3 / 2, //   Aspect Ration
             crossAxisSpacing: 20, // Horizontal Spacing
-            mainAxisSpacing: 20 // Vertical Spacing
+            mainAxisSpacing: 20, // Vertical Spacing
           ),
           children: [
             for (final category in availableCategories)
-              CategoryGridItem(category: category)
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                },
+              ),
           ],
         ),
       ),
