0a70c3df488e83511a555d22940257eb985038eb
RaaidK47 <raaid.khan47@gmail.com>
2025-10-07 21:45:29 +0500

    Completed Meals App - Basic

--- meals_app/lib/screens/tabs.dart
+++ meals_app/lib/screens/tabs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
@@ -13,8 +15,17 @@ class TabsScreen extends StatefulWidget {
   }
 }
 
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
+
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
+
+  Map<Filter, bool> _selectedFilters = kInitialFilters;
 
   void _selectPage(int index) {
     setState(() {
@@ -26,9 +37,9 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
   }
 
   void _toggleMealFavouriteStatus(Meal meal) {
@@ -39,9 +50,7 @@ class _TabsScreenState extends State<TabsScreen> {
         _favouriteMeals.remove(meal);
         _showInfoMessage("Meal is No Longer a Favourite");
       });
-      
-    }
-    else {
+    } else {
       setState(() {
         _favouriteMeals.add(meal);
         _showInfoMessage("Marked as a Favourite");
@@ -50,31 +59,59 @@ class _TabsScreenState extends State<TabsScreen> {
     // _favouriteMeals.forEach((meal) => print(meal.title));
   }
 
-  void _setScreen(String identifier) {
-    if (identifier == 'filters'){
+  Future<void> _setScreen(String identifier) async {
+    Navigator.of(context).pop(); //Closing Drawer
+    if (identifier == 'filters') {
       // We will Add Filters Screen later
-    } else {
-      // Else we go to Meals Screen
-      // If we want to go to Meals screen, we have to close drawer first
-      Navigator.of(context).pop(); //Closing Drawer
+      // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx) => FiltersScreen()));
+      final results = await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(builder: (ctx) => FiltersScreen(currentFilters: _selectedFilters,)),
+      );
+      setState(() {
+        _selectedFilters = results ?? kInitialFilters;
+      });
+
+      // print(results);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget activePage = CategoriesScreen(onToggleFavourite: _toggleMealFavouriteStatus,);
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+
+    Widget activePage = CategoriesScreen(
+      onToggleFavourite: _toggleMealFavouriteStatus,
+      availableMeals: availableMeals,
+    );
     var activePageTitle = "Categories";
 
     if (_selectedPageIndex == 1) {
       // We are re-using MealsScreen for Favourites
       // Not Setting the Title
-      activePage = MealsScreen(onToggleFavourite: _toggleMealFavouriteStatus, meals: _favouriteMeals);
+      activePage = MealsScreen(
+        onToggleFavourite: _toggleMealFavouriteStatus,
+        meals: _favouriteMeals,
+      );
       activePageTitle = "Your Favourites";
     }
 
     return Scaffold(
       appBar: AppBar(title: Text(activePageTitle)),
-      drawer: MainDrawer(onSelectScreen: _setScreen,),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         // index > Index of Tab in BottomNavigationBar
