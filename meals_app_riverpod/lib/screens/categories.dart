d9c68df25bb9b7eb7aa39e2a4e6379c873cd845e
RaaidK47 <raaid.khan47@gmail.com>
2025-10-08 21:58:55 +0500

    Added meals_app_riverpod App and used basic Riverpod Provider for Static Data i.e. dummyMeals

---
+++ meals_app_riverpod/lib/screens/categories.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key, required this.onToggleFavourite, required this.availableMeals});
+
+  final List<Meal> availableMeals;
+
+  final void Function(Meal meal) onToggleFavourite;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    // Context is not globally available in Stateless Widget
+    // [Similar] Navigator.push(context, route);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(onToggleFavourite: onToggleFavourite, title: category.title, meals: filteredMeals),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Two Columns in Grid
+              childAspectRatio: 3 / 2, //   Aspect Ration
+              crossAxisSpacing: 20, // Horizontal Spacing
+              mainAxisSpacing: 20, // Vertical Spacing
+            ),
+            children: [
+              for (final category in availableCategories)
+                CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ),
+            ],
+          ),
+    );
+  }
+}
