292ee92b1397b0d778b7faedd98d49bb0900e242
RaaidK47 <raaid.khan47@gmail.com>
2025-10-13 19:22:11 +0500

    Added Implicit Animations - Turning Star Button & Transitions between Screens

--- meals_app_animations/lib/screens/meal_details.dart
+++ meals_app_animations/lib/screens/meal_details.dart
@@ -22,25 +22,46 @@ class MealDetailsScreen extends ConsumerWidget {
               final wasAdded = ref
                   .read(favouriteMealsProvider.notifier)
                   .toggleMealFavouriteStatus(meal);
-              
+
               ScaffoldMessenger.of(context).clearSnackBars();
 
-              ScaffoldMessenger.of(
-                context,
-              ).showSnackBar(SnackBar(content: Text(wasAdded ? "Marked as a Favourite" : "Meal is No Longer a Favourite")));
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    wasAdded
+                        ? "Marked as a Favourite"
+                        : "Meal is No Longer a Favourite",
+                  ),
+                ),
+              );
             },
-            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.5, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavourite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavourite),
+              ),
+            ),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 200,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 14),
             Text(
