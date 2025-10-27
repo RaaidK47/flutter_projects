618bec00124fe06d8d0b8dfcb8fdf34879f78a65
RaaidK47 <raaid.khan47@gmail.com>
2025-10-27 19:08:36 +0500

    Create favourite_palces_app and create base version of the App

---
+++ favourite_places_app/lib/providers/user_places.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favourite_places_app/models/place.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
